74b596a0c1e09f0d7f55334de54828209a1e2f54
funky-furry <enf_maro@hotmail.com>
2022-03-29 19:48:53 -0300

    adicionando balão e som

--- sketch.js
+++ sketch.js
@@ -15,8 +15,10 @@ var fruit;
 var fruitcon;
 var backgroundImage, melonImage, rabbitImage;
 var rabbit;
-var button;
+var button, buttonmute;
 var sadbunny, eatingbunny, idlebunny;
+var eatingbunnysound, ropecut, sadbunnysound, backgroundsound, balloonsound;
+var balloon;
 
 function preload(){
   backgroundImage = loadImage("./images/background.png");
@@ -25,6 +27,11 @@ function preload(){
   sadbunny = loadAnimation("./images/sad_1.png","./images/sad_2.png","./images/sad_3.png");
   eatingbunny = loadAnimation("./images/eat_0.png","./images/eat_1.png","./images/eat_2.png","./images/eat_3.png","./images/eat_4.png");
   idlebunny = loadAnimation("./images/blink_1.png", "./images/blink_2.png", "./images/blink_3.png");
+  eatingbunnysound = loadSound("./sound/eating_sound.mp3");
+  ropecut = loadSound("./sound/rope_cut.mp3");
+  backgroundsound = loadSound("./sound/sound1.mp3");
+  sadbunnysound = loadSound("./sound/sad.wav");
+  balloonsound = loadSound("./sound/air.wav");
   sadbunny.playing = true;
   sadbunny.frameDelay = 10;
   sadbunny.looping = false;
@@ -33,11 +40,14 @@ function preload(){
   eatingbunny.frameDelay = 10;
   idlebunny.playing = true;
   idlebunny.frameDelay = 10;
+  
 }
 
 function setup() 
 {
   createCanvas(500,800);
+  backgroundsound.play();
+  backgroundsound.setVolume(0.2);
   frameRate(80);
   engine = Engine.create();
   world = engine.world;
@@ -64,6 +74,16 @@ function setup()
   button.position(220,5);
   button.size(50,50);
   button.mouseClicked(drop);
+  
+  buttonmute = createImg("./images/mute.png");
+  buttonmute.position(450,5);
+  buttonmute.size(50,50);
+  buttonmute.mouseClicked(mute);
+
+  balloon = createImg("./images/balloon.png");
+  balloon.position(10,290);
+  balloon.size(150,100);
+  balloon.mouseClicked(ballonforce);
 }
 
 function draw() 
@@ -84,10 +104,16 @@ function draw()
   drawSprites();
   if(collide(fruit, rabbit)){
     rabbit.changeAnimation("eating");
+    eatingbunnysound.play();
   }
-  if(collide(fruit, ground.body)){
-    rabbit.changeAnimation("sad");
+  
+  if(fruit) {
+    if(fruit.position.y > 760){
+      rabbit.changeAnimation("sad");
+      sadbunnysound.play();
+    }
   }
+  
 }
 
 function drop () {
@@ -106,4 +132,16 @@ function collide(bodyA, bodyB) {
       return false;
     }
   }
+}
+
+function mute() {
+  if(backgroundsound.isPlaying()){
+    backgroundsound.stop();
+  }else{
+    backgroundsound.play();
+  }
+}
+
+function ballonforce() {
+  Body.applyForce(fruit, {x:0, y:0}, {x:0.01, y:0})
 }
